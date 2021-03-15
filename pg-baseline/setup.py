77cc74ab727b9789c122a249f6040b01ed5c6a01
Rodrigo Chau <rodifelix94@gmail.com>
2021-03-15 15:42:21 +0100

    Added setup.py

---
+++ pg-baseline/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='pg_baseline',
+      version='0.0.1',
+      install_requires=['stable-baselines3', 'gym', 'numpy', 'torch', 'cloudpickle', 'pandas', 'matplotlib', 'torchvision']
+)
