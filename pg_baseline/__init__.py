aacd4f9bdab7d9e3aea1d9d8e4614950ef08a6a8
Rodrigo Chau <rodifelix94@gmail.com>
2021-03-15 16:27:39 +0100

    Fixed names of imports

--- pg_baseline/__init__.py
+++ pg_baseline/__init__.py
@@ -1,3 +1,3 @@
-from pg_baseline.pg_dqn import DQN
-from pg_baseline.pg_policies import CnnPolicy, MlpPolicy
+from pg_baseline.pg_dqn import PGDQN
+from pg_baseline.pg_policies import PGDQNPolicy
 from pg_baseline.pg_densenet import PGdensenet121
