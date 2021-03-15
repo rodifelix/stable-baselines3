e3309c561d21fc1c4ce2c2c6b83e7055741245b6
Rodrigo Chau <rodifelix94@gmail.com>
2021-03-15 17:10:32 +0100

    Added version tag to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='pg_baseline',
       version='0.0.1',
-      install_requires=['stable-baselines3', 'gym', 'numpy', 'torch', 'cloudpickle', 'pandas', 'matplotlib', 'torchvision']
+      install_requires=['stable-baselines3==0.10.0', 'gym', 'numpy', 'torch', 'cloudpickle', 'pandas', 'matplotlib', 'torchvision']
 )
