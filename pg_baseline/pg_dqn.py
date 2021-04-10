7f07d5e2ed06e1bf6bb9ab38c411cd5ae8cc67a7
Rodrigo Chau <rodifelix94@gmail.com>
2021-04-10 11:00:48 +0200

    Changed forward type to specific rotations to save computation time

--- pg_baseline/pg_dqn.py
+++ pg_baseline/pg_dqn.py
@@ -157,12 +157,12 @@ class PGDQN(OffPolicyAlgorithm):
 
             with th.no_grad():
                 # Select best estimated next action
-                _, action_idx = self.q_net.forward(replay_data.next_observations).max(dim=1).unsqueeze(0).T
+                _, action_idx = self.q_net.forward(replay_data.next_observations).max(dim=1)
+                action_idx = action_idx.unsqueeze(0).T
                 # Compute the target Q values
-                # forward type: batch size images, each with 16 rotations
-                target_q = self.q_net_target.forward(replay_data.next_observations)
+                target_q = self.q_net_target.forward_specific_rotations(replay_data.next_observations, th.floor_divide(action_idx.long(), self.heightmap_resolution*self.heightmap_resolution))
                 # Evaluate action with target network
-                target_q = target_q.gather(dim=1, index=action_idx.long())
+                target_q = target_q.gather(dim=1, index=th.remainder(action_idx.long(), self.heightmap_resolution*self.heightmap_resolution))
                 # Avoid potential broadcast issue
                 target_q = target_q.reshape(-1, 1)
                 # 1-step TD target
