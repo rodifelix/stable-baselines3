60a1f1a0da0caa7be155916345d890bca6f347f0
Rodrigo Chau <rodifelix94@gmail.com>
2021-04-21 13:38:48 +0200

    Add pg_hourglass to Init

--- pg_baseline/__init__.py
+++ pg_baseline/__init__.py
@@ -2,3 +2,4 @@ from pg_baseline.pg_dqn import PGDQN
 from pg_baseline.pg_policies import PGDQNPolicy
 from pg_baseline.pg_densenet import PGdensenet121
 from pg_baseline.pg_buffer import PGBuffer
+from pg_baseline.pg_hourglass import Push_Into_Box_Net, get_pibn_parameters
