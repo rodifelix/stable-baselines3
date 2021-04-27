08988bb3e023cfdd52043659e4244d7dbb225c00
Rodrigo Chau <rodifelix94@gmail.com>
2021-04-27 16:36:17 +0200

    Removed color channels from input channels

--- pg_baseline/pg_hourglass.py
+++ pg_baseline/pg_hourglass.py
@@ -270,6 +270,6 @@ def get_pibn_parameters():
         # how many hourglass modules
         'n_stages': 1,
         # channels of the image
-        'input_channels': 4,
+        'input_channels': 1,
         # output channels
         'output_channels': 16}
