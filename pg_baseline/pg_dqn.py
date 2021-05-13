df921d71dd9fa05c72a2e7c78ae05f24889280cc
Rodrigo Chau <rodifelix94@gmail.com>
2021-05-13 17:49:39 +0200

    Remove prioritized replay buffer

--- pg_baseline/pg_dqn.py
+++ pg_baseline/pg_dqn.py
@@ -10,7 +10,6 @@ from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
 from stable_baselines3.common.utils import get_linear_fn, polyak_update
 from pg_baseline.pg_policies import PGDQNPolicy
-from pg_baseline.pg_buffer import PGBuffer
 
 
 class PGDQN(OffPolicyAlgorithm):
@@ -124,22 +123,7 @@ class PGDQN(OffPolicyAlgorithm):
             self._setup_model()
 
     def _setup_model(self) -> None:
-        self._setup_lr_schedule()
-        self.set_random_seed(self.seed)
-        self.replay_buffer = PGBuffer(
-            self.buffer_size,
-            self.observation_space,
-            self.action_space,
-            self.device,
-            optimize_memory_usage=self.optimize_memory_usage,
-        )
-        self.policy = self.policy_class(
-            self.observation_space,
-            self.action_space,
-            self.lr_schedule,
-            **self.policy_kwargs  # pytype:disable=not-instantiable
-        )
-        self.policy = self.policy.to(self.device)
+        super(PGDQN, self)._setup_model()
         self._create_aliases()
         self.exploration_schedule = get_linear_fn(
             self.exploration_initial_eps, self.exploration_final_eps, self.exploration_fraction
@@ -204,9 +188,6 @@ class PGDQN(OffPolicyAlgorithm):
 
             # Retrieve the q-values for the actions from the replay buffer
             current_q = th.gather(current_q, dim=1, index=replay_data.actions.long())
-
-            new_surprise_values = np.abs(current_q.detach().cpu().numpy() - target_q.detach().cpu().numpy())
-            self.replay_buffer.update_sample_surprise_values(new_surprise_values)
 
             # Compute Huber loss (less sensitive to outliers)
             loss = F.mse_loss(current_q, target_q)
