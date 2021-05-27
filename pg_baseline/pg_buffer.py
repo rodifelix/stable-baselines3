e9aaebc35b21600579b12f6ea69a1e129eedde15
Rodrigo Chau <rodifelix94@gmail.com>
2021-05-27 15:08:02 +0200

    Implement uniform sampling from rotations

--- pg_baseline/pg_buffer.py
+++ pg_baseline/pg_buffer.py
@@ -65,13 +65,13 @@ class PGBuffer(ReplayBuffer):
         self.actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=action_space.dtype)
         self.rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
         self.dones = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
-        self.surprise = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.rotations = np.zeros((self.buffer_size, self.n_envs), dtype=np.int32)
         self.save_indices = []
         self.iteration = np.zeros((self.buffer_size, self.n_envs), dtype=np.int32)
         self.iteration_offset = 0
 
         if psutil is not None:
-            total_memory_usage = self.observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes + self.surprise.nbytes + self.iteration.nbytes
+            total_memory_usage = self.observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes + self.rotation.nbytes + self.iteration.nbytes
             if self.next_observations is not None:
                 total_memory_usage += self.next_observations.nbytes
 
@@ -93,6 +93,7 @@ class PGBuffer(ReplayBuffer):
             self.next_observations[self.pos] = np.array(next_obs).copy()
 
         self.actions[self.pos] = np.array(action).copy()
+        self.rotations[self.pos] = np.array(action).copy() // (self.obs_shape[1]*self.obs_shape[2])
         self.rewards[self.pos] = np.array(reward).copy()
         self.dones[self.pos] = np.array(done).copy()
         self.iteration[self.pos] = [self.iteration_offset*self.buffer_size + self.pos]
@@ -103,6 +104,43 @@ class PGBuffer(ReplayBuffer):
             self.pos = 0
             self.iteration_offset += 1
 
+    def sample(self, batch_size: int, env: Optional[VecNormalize] = None) -> PGBufferSamples:
+        """
+        Sample elements from the replay buffer.
+        Custom sampling when using memory efficient variant,
+        as we should not sample the element with index `self.pos`
+        See https://github.com/DLR-RM/stable-baselines3/pull/28#issuecomment-637559274
+
+        :param batch_size: Number of element to sample
+        :param env: associated gym VecEnv
+            to normalize the observations/rewards when sampling
+        :return:
+        """
+        if not self.optimize_memory_usage:
+            upper_bound = self.buffer_size if self.full else self.pos
+            batch_size = min(upper_bound, batch_size)
+
+            indices_per_rotation = self._get_indices_per_rotation()
+
+            sample_indices = None
+            for indices in indices_per_rotation:
+                index = indices[0]
+                if index.shape[0] > 0:
+                    if sample_indices is None:
+                        sample_indices = index[np.random.randint(0, index.shape[0], size=1)].copy()
+                    else:
+                        sample_indices = np.append(sample_indices, index[np.random.randint(0, index.shape[0], size=1)])
+
+            return self._get_samples(sample_indices, env=env)
+        else:
+            raise NotImplementedError("Optimize memory usage with prio replay not supported yet")
+        #TODO: implement for optimize memory usage
+        """if self.full:
+            batch_inds = (np.random.randint(1, self.buffer_size, size=batch_size) + self.pos) % self.buffer_size
+        else:
+            batch_inds = np.random.randint(0, self.pos, size=batch_size)
+        return self._get_samples(batch_inds, env=env)"""   
+    
     def _get_samples(self, batch_inds: np.ndarray, env: Optional[VecNormalize] = None) -> PGBufferSamples:
         if self.optimize_memory_usage:
             next_obs = self._normalize_obs(self.observations[(batch_inds + 1) % self.buffer_size, 0, :], env)
@@ -118,6 +156,12 @@ class PGBuffer(ReplayBuffer):
             self.iteration[batch_inds]
         )
         return PGBufferSamples(*tuple(map(self.to_torch, data)))
+
+
+    def _get_indices_per_rotation(self):
+        upper_bound = self.buffer_size if self.full else self.pos
+        indices_per_rotation = [np.nonzero(self.rotations[:upper_bound] == i) for i in range(8)]
+        return indices_per_rotation
 
 
 
