611d33808dc9af73a76f5d2e28fbcd5aca63e775
Rodrigo Chau <rodifelix94@gmail.com>
2021-05-27 15:07:40 +0200

    Revert "Reintroduce pg buffer but remove prio sampling"
    This reverts commit 83e0bb97188a322376642ca361ccc6b64fb45c35.

--- pg_baseline/pg_buffer.py
+++ pg_baseline/pg_buffer.py
@@ -103,6 +103,51 @@ class PGBuffer(ReplayBuffer):
             self.pos = 0
             self.iteration_offset += 1
 
+
+    def sample(self, batch_size: int, env: Optional[VecNormalize] = None) -> PGBufferSamples:
+        """
+        Sample elements from the replay buffer.
+        Custom sampling when using memory efficient variant,
+        as we should not sample the element with index `self.pos`
+        See https://github.com/DLR-RM/stable-baselines3/pull/28#issuecomment-637559274
+
+        :param batch_size: Number of element to sample
+        :param env: associated gym VecEnv
+            to normalize the observations/rewards when sampling
+        :return:
+        """
+        if not self.optimize_memory_usage:
+            upper_bound = self.buffer_size if self.full else self.pos
+            batch_size = min(upper_bound, batch_size)
+            most_recent_element_idx = self.buffer_size - 1 if self.pos == 0 else self.pos-1
+            assert batch_size > 0, "Error: Nothing in buffer to sample or batch_size set to 0" 
+            if batch_size > 1:                
+                sorted_surprise_ind = np.argsort(self.surprise[:upper_bound,0]).astype(int)
+                #skip element most recent element at self.pos-1, as this will always be selected
+                sorted_surprise_ind = sorted_surprise_ind[sorted_surprise_ind != most_recent_element_idx]
+                for i in range(batch_size-1):
+                    rand_sample_ind = np.round(np.random.power(2, 1)*(sorted_surprise_ind.size-1)).astype(int)
+                    if i == 0:
+                        self.save_indices = sorted_surprise_ind[rand_sample_ind]
+                    else:
+                        self.save_indices = np.append(self.save_indices, sorted_surprise_ind[rand_sample_ind])
+                    sorted_surprise_ind = np.delete(sorted_surprise_ind, rand_sample_ind)
+
+                # Always sample the most recent entry to generate surprise value
+                self.save_indices = np.append(self.save_indices, [most_recent_element_idx])
+            else:
+                self.save_indices = [most_recent_element_idx]
+
+            return self._get_samples(self.save_indices, env=env)
+        else:
+            raise NotImplementedError("Optimize memory usage with prio replay not supported yet")
+        #TODO: implement for optimize memory usage
+        """if self.full:
+            batch_inds = (np.random.randint(1, self.buffer_size, size=batch_size) + self.pos) % self.buffer_size
+        else:
+            batch_inds = np.random.randint(0, self.pos, size=batch_size)
+        return self._get_samples(batch_inds, env=env)"""
+
     def _get_samples(self, batch_inds: np.ndarray, env: Optional[VecNormalize] = None) -> PGBufferSamples:
         if self.optimize_memory_usage:
             next_obs = self._normalize_obs(self.observations[(batch_inds + 1) % self.buffer_size, 0, :], env)
@@ -118,6 +163,11 @@ class PGBuffer(ReplayBuffer):
             self.iteration[batch_inds]
         )
         return PGBufferSamples(*tuple(map(self.to_torch, data)))
+
+    def update_sample_surprise_values(self, new_values: np.ndarray):
+        assert len(new_values) == len(self.save_indices), "Amount of saved indices and provided amount of new values not the same"
+        self.surprise[self.save_indices] = new_values.copy()
+        self.save_indices = []
 
 
 
--- pg_baseline/pg_dqn.py
+++ pg_baseline/pg_dqn.py
@@ -10,7 +10,6 @@ from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
 from stable_baselines3.common.utils import get_linear_fn, polyak_update
 from pg_baseline.pg_policies import PGDQNPolicy
-from pg_baseline.pg_buffer import PGBuffer
 
 
 class PGDQN(OffPolicyAlgorithm):
@@ -126,24 +125,7 @@ class PGDQN(OffPolicyAlgorithm):
             self._setup_model()
 
     def _setup_model(self) -> None:
-        self._setup_lr_schedule()
-        self.set_random_seed(self.seed)
-        self.replay_buffer = PGBuffer(
-            self.buffer_size,
-            self.observation_space,
-            self.action_space,
-            self.device,
-            optimize_memory_usage=self.optimize_memory_usage,
-        )
-        self.policy = self.policy_class(
-            self.observation_space,
-            self.action_space,
-            self.lr_schedule,
-            **self.policy_kwargs  # pytype:disable=not-instantiable
-        )
-        self.policy = self.policy.to(self.device)
-
-
+        super(PGDQN, self)._setup_model()
         self._create_aliases()
         self.exploration_schedule = get_linear_fn(
             self.exploration_initial_eps, self.exploration_final_eps, self.exploration_fraction
