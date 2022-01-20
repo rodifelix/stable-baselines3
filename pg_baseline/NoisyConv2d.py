0caa669f8943b00e77073bd86da927bd240dd1af
Rodrigo Chau <rodifelix94@gmail.com>
2022-01-20 15:20:14 +0100

    Only allow factorised Noise. Std_init default is 0.5. Add explicit reset_noise. TODO: Check reset_noise

--- pg_baseline/NoisyConv2d.py
+++ pg_baseline/NoisyConv2d.py
@@ -1,11 +1,3 @@
-"""
-A noisy convolution 2d for pytorch
-Adapted from:
-- https://raw.githubusercontent.com/Scitator/Run-Skeleton-Run/master/common/modules/NoisyLinear.py
-- https://github.com/pytorch/pytorch/pull/2103/files#diff-531f4c06f42260d699f43dabdf741b6d
-More details can be found in the paper `Noisy Networks for Exploration`
-Original: https://gist.github.com/wassname/001aff274c7c8196055fabfc06cf80c5
-"""
 import math
 
 import torch
@@ -39,7 +31,7 @@ class NoisyConv2d(Module):
         >>> print(output.size())
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size, bias=True, stride=1, padding=1, dilation=1, groups=1, factorised=True, std_init=None):
+    def __init__(self, in_channels, out_channels, kernel_size, bias=True, stride=1, padding=0, dilation=1, groups=1, std_init=0.5):
         super(NoisyConv2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -48,66 +40,62 @@ class NoisyConv2d(Module):
         self.padding = _pair(padding)
         self.dilation = _pair(dilation)
         self.groups = groups
-        self.factorised = factorised
+        self.std_init = std_init
         
-        self.weight_mu = Parameter(torch.Tensor(out_channels, in_channels//groups, *kernel_size))
-        self.weight_sigma = Parameter(torch.Tensor(out_channels, in_channels//groups, *kernel_size))
+        self.weight_mu = Parameter(torch.Tensor(out_channels, in_channels//groups, *self.kernel_size))
+        self.weight_sigma = Parameter(torch.Tensor(out_channels, in_channels//groups, *self.kernel_size))
         if bias:
             self.bias_mu = Parameter(torch.Tensor(out_channels))
             self.bias_sigma = Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter('bias', None)
-        if not std_init:
-            if self.factorised:
-                self.std_init = 0.4
-            else:
-                self.std_init = 0.017
-        else:
-            self.std_init = std_init
+
         self.reset_parameters(bias)
+        self.reset_noise()
 
     def reset_parameters(self, bias):
-        if self.factorised:
-            mu_range = 1. / math.sqrt(self.weight_mu.size(1))
-            self.weight_mu.data.uniform_(-mu_range, mu_range)
-            self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.weight_sigma.size(1)))
-            if bias:
-                self.bias_mu.data.uniform_(-mu_range, mu_range)
-                self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.bias_sigma.size(0)))
-        else:
-            mu_range = math.sqrt(3. / self.weight_mu.size(1))
-            self.weight_mu.data.uniform_(-mu_range, mu_range)
-            self.weight_sigma.data.fill_(self.std_init)
-            if bias:
-                self.bias_mu.data.uniform_(-mu_range, mu_range)
-                self.bias_sigma.data.fill_(self.std_init)
+        mu_range = 1. / math.sqrt(self.weight_mu.size(1))
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.weight_sigma.size(1)))
+        if bias:
+            self.bias_mu.data.uniform_(-mu_range, mu_range)
+            self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.bias_sigma.size(0)))
 
     def scale_noise(self, size):
-        x = torch.Tensor(size).normal_()
+        x = torch.randn(size)
         x = x.sign().mul(x.abs().sqrt())
         return x
 
+    def reset_noise(self):
+        """Make new noise."""
+        epsilon = None
+        for dim in self.weight_sigma.size():
+            if epsilon is None:
+                epsilon = self.scale_noise(dim)
+            else:
+                epsilon = epsilon.unsqueeze(-1)*self.scale_noise(dim)
+        self.weight_epsilon = Variable(epsilon)
+        self.bias_epsilon = Variable(self.scale_noise(self.out_channels))
+
     def forward(self, input):
-        if self.factorised:
-            epsilon = None
-            for dim in self.weight_sigma.size():
-                if epsilon is None:
-                    epsilon = self.scale_noise(dim)
-                else:
-                    epsilon = epsilon.unsqueeze(-1)*self.scale_noise(dim)
-            weight_epsilon = Variable(epsilon)
-            bias_epsilon = Variable(self.scale_noise(self.out_channels))
+        if self.training:
+            return F.conv2d(input,
+                            self.weight_mu + self.weight_sigma.mul(self.weight_epsilon),
+                            self.bias_mu + self.bias_sigma.mul(self.bias_epsilon),
+                            stride=self.stride,
+                            padding=self.padding,
+                            dilation=self.dilation,
+                            groups=self.groups
+                        )
         else:
-            weight_epsilon = Variable(torch.Tensor(self.out_channels, self.in_channels, *self.kernel_size).normal_())
-            bias_epsilon = Variable(torch.Tensor(self.out_channels).normal_())
-        return F.conv2d(input,
-                        self.weight_mu + self.weight_sigma.mul(weight_epsilon),
-                        self.bias_mu + self.bias_sigma.mul(bias_epsilon),
-                        stride=self.stride,
-                        padding=self.padding,
-                        dilation=self.dilation,
-                        groups=self.groups
-                       )
+            return F.conv2d(input,
+                            self.weight_mu,
+                            self.bias_mu,
+                            stride=self.stride,
+                            padding=self.padding,
+                            dilation=self.dilation,
+                            groups=self.groups
+                        )
 
     def __repr__(self):
         s = ('{name}({in_channels}, {out_channels}, kernel_size={kernel_size}'
