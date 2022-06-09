e224a889cd35ca9542d0706cf0cb11491ecea562
Rodrigo Chau <rodifelix94@gmail.com>
2022-06-09 11:06:31 +0200

    Added new LightHG network policy. TODO: check optimizer params

--- pg_baseline/pg_policies.py
+++ pg_baseline/pg_policies.py
@@ -7,10 +7,146 @@ import torch as th
 from torch import nn
 from collections import OrderedDict
 import numpy as np
-from pg_baseline import pg_hourglass, pg_mask_net, pg_densenet
+from pg_baseline import pg_hourglass, pg_light_multi_head_hourglass, pg_mask_net, pg_densenet
 from torch.autograd import Variable
 import torch.nn.functional as F
 from stable_baselines3.common.policies import BasePolicy, register_policy
+
+class LightHGNetwork(BasePolicy):
+    """
+    Action-Value (Q-Value) network for DQN
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param net_arch: The specification of the policy and value networks.
+    :param activation_fn: Activation function
+    :param normalize_images: Whether to normalize images or not,
+         dividing by 255.0 (True by default)
+    """
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        heightmap_resolution: int,
+        num_rotations: int,
+        ucb_confidence: float,
+        preload_path: Optional[str],
+        dueling: bool,
+        noisy: bool,
+    ):
+        super(LightHGNetwork, self).__init__(
+            observation_space,
+            action_space,
+            normalize_images=False,
+        )
+        self.heightmap_resolution = heightmap_resolution
+
+        self.num_rotations = num_rotations
+
+        self.net = pg_light_multi_head_hourglass.Push_Into_Box_Net(device = self.device, img_shape = [heightmap_resolution, heightmap_resolution], num_out_feat_channels=num_rotations)
+        self.ucb_confidence = ucb_confidence
+
+        if self.ucb_confidence > 0:
+            self.timestep = 1
+            self.action_counter = np.ones((self.num_rotations), dtype=np.int)
+
+        self.preload_path = preload_path
+
+        if self.preload_path is not None:
+            state_dict = th.load(preload_path)
+            self.net.load_state_dict(state_dict, strict=True)
+
+    def _get_data(self) -> Dict[str, Any]:
+        data = super()._get_data()
+
+        data.update(
+            dict(
+                heightmap_resolution=self.heightmap_resolution,
+                num_rotations=self.num_rotations,
+                ucb_confidence=self.ucb_confidence,
+                preload_path=self.preload_path,  
+            )
+        )
+        return data
+
+    def forward(self, obs: th.Tensor, mask=True) -> th.Tensor:
+        """
+        Predict the q-values.
+        :param obs: Observation. Expects Tensor with shape N x C x H x W
+        :return: The estimated Q-Value for each action.
+        """
+        batch_size = obs.shape[0]
+
+        output_prob = self.net.forward(obs.to(self.device))
+
+        if mask:
+            output_prob = output_prob[0]
+        else:
+            output_prob = output_prob[1]
+
+        return th.reshape(output_prob, (batch_size, self.num_rotations*self.heightmap_resolution*self.heightmap_resolution))
+
+
+    def mask(self, obs: th.Tensor) -> th.Tensor:
+        mask_output = self.net.forward(obs)[2]
+        return th.reshape(mask_output, (obs.shape[0], self.num_rotations*self.heightmap_resolution*self.heightmap_resolution))
+
+    
+    def _predict(self, observation: th.Tensor, deterministic: bool = True) -> th.Tensor:        
+        if deterministic:
+            q_values = self.forward(observation)
+            if self.ucb_confidence > 0:
+                # UCB
+                q_values = np.reshape(q_values.detach().cpu().numpy(), (self.num_rotations, -1))
+                action_idxs = q_values.argmax(axis=1)
+                actions = q_values.max(axis=1)
+                confidence_value = self.ucb_confidence * np.sqrt(np.log(self.timestep)/self.action_counter)
+                actions += confidence_value
+                rotation = actions.argmax()
+                action = rotation*self.heightmap_resolution*self.heightmap_resolution + action_idxs[rotation]
+                action = th.tensor([action])
+                self.action_counter[rotation] += 1
+                self.timestep += 1
+            else:
+                action = q_values.argmax(dim=1)            
+            
+        else:
+            actions = th.arange(start=0, end=self.num_rotations*self.heightmap_resolution*self.heightmap_resolution, dtype=th.long).to(self.device)
+            actions = actions.reshape((1, self.num_rotations, self.heightmap_resolution, self.heightmap_resolution))
+
+            masked_actions = self.explore_mask(observation, actions)
+
+            valid_idx = masked_actions[masked_actions >= 0]
+            if len(valid_idx) > 0:
+                choice = np.random.randint(low=0, high=len(valid_idx))
+                valid_idx = valid_idx.type(th.long)
+                action = valid_idx[choice].reshape(-1)
+            else:
+                raise NoObjectsInSceneException("No valid actions after mask. Is scene empty?")
+
+            print("\n\nExploring in next iteration: Random action index:", action)
+        return action
+
+    def explore_mask(self, obs: th.Tensor, output_prob: th.Tensor):
+        threshhold_depth = 0.01
+        depth_min = 0.0
+        depth_max = 0.1
+        threshold_norm = (threshhold_depth - depth_min)/(depth_max - depth_min)
+
+        masked_input = obs > threshold_norm
+        masked_input = masked_input.type(th.float).to(self.device)
+        diluted_mask = th.nn.functional.max_pool2d(input=masked_input,kernel_size=(34,34),stride=(1,1),padding=17)
+
+        diluted_mask = th.narrow(diluted_mask, 2, 1, self.heightmap_resolution)
+        diluted_mask = th.narrow(diluted_mask, 3, 1, self.heightmap_resolution)
+
+        diluted_mask = diluted_mask - 1.
+        diluted_mask = diluted_mask * th.finfo(th.float).max
+
+        #diluted_mask = th.nan_to_num(diluted_mask)
+
+        diluted_mask = th.repeat_interleave(diluted_mask, self.num_rotations, dim=1)
+
+        return output_prob + diluted_mask
 
 class HGNetwork(BasePolicy):
     """
@@ -447,6 +583,7 @@ class PGDQNPolicy(BasePolicy):
             "dueling": self.dueling,
             "noisy": self.noisy,
         }
+        
         if self.net_class == "HG_Mask":
             self.net_args["preload_mask_path"] = self.preload_mask_path
             self.net_args["use_masknet"] = True
@@ -454,6 +591,8 @@ class PGDQNPolicy(BasePolicy):
         elif self.net_class == "HG":
             self.net_args["preload_mask_path"] = None
             self.net_args["use_masknet"] = False
+        elif self.net_class == "LightHG":
+            self.net_args["preload_path"] = self.preload_mask_path
 
         self.q_net, self.q_net_target = None, None
         self._build(lr_schedule)
@@ -482,6 +621,8 @@ class PGDQNPolicy(BasePolicy):
     def make_q_net(self) -> BasePolicy:
         if self.net_class == "HG" or self.net_class == "HG_Mask":
             return HGNetwork(**self.net_args).to(self.device)
+        elif self.net_class == "LightHG":
+            return LightHGNetwork(**self.net_args).to(self.device)
         elif self.net_class == "VPG":
             return VPGNetwork(**self.net_args).to(self.device)
 
